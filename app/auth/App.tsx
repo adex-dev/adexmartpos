bc09be1568ef7b1e0b055b4c312535e6dee1c7fd
akmadnudin <akmadnudin@indobara.co.id>
2026-04-23 16:02:58 +0700

    Pembuatan page login

--- app/auth/App.tsx
+++ app/auth/App.tsx
@@ -1,14 +1,49 @@
 import logo from '../assets/img/login.webp';
 import { Form, Input, Button, Title } from '../components/ui';
-import { formatRupiah } from '../components/utils/currency';
 import { useState, useEffect } from 'react';
 
 export default function App() {
-  const [price, setPrice] = useState('');
+  const [err, setErr] = useState('');
+  const [form, setForm] = useState({
+    username: '',
+    password: '',
+  });
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (typeof name !== 'string' || name.trim() === '') {
+      setErr('Periksa kembali username dan password anda.!');
+      return;
+    }
+    setForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (typeof form.username !== 'string' || form.username.trim() === '') {
+      setErr('Periksa kembali username anda.!');
+      return;
+    }
+    if (typeof form.password !== 'string' || form.password.trim() === '') {
+      setErr('Periksa kembali password anda.!');
+      return;
+    }
+    setErr('');
+    console.log(form);
+    // { email: "...", password: "..." }
+  };
   useEffect(() => {
-    setPrice(formatRupiah(100000));
-  }, []);
+    if (!err) return;
+
+    const timer = setTimeout(() => {
+      setErr(''); // hapus error setelah 3 detik
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [err]);
   return (
     <>
       <div className="hero bg-base-200 min-h-screen">
@@ -31,14 +66,25 @@ export default function App() {
           <div className="card bg-base-100 w-full max-w-md shrink-0 shadow-2xl">
             <div className="card-body">
               <Title size="lg">Login</Title>
-              <h3>{price}</h3>
               <fieldset className="fieldset">
-                <Form onSubmit={(e) => e.preventDefault()}>
-                  <Input label="Email" type="email" placeholder="email..." />
+                <Form onSubmit={handleSubmit}>
                   <Input
+                    autoComplete="off"
+                    label="Username"
+                    type="text"
+                    name="username"
+                    placeholder="Username..."
+                    value={form.username}
+                    onChange={handleChange}
+                  />
+                  <Input
+                    autoComplete="off"
                     label="Password"
+                    name="password"
                     type="password"
                     placeholder="password"
+                    value={form.password}
+                    onChange={handleChange}
                   />
                   <Button
                     className="btn btn-accent text-white"
@@ -47,6 +93,11 @@ export default function App() {
                   >
                     Submit
                   </Button>
+                  {err && (
+                    <div role="alert" className="alert alert-error alert-dash">
+                      <span>{err}</span>
+                    </div>
+                  )}
                 </Form>
               </fieldset>
             </div>
